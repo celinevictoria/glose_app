43dc8651bd4004e5bd3b0bfb87f33d23a9380317
Celine Victoria <celine.victoria@hotmail.no>
2023-09-30 10:57:40 +0200

    switch to use new widget

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'game_page.dart';
+import 'start_button.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({
@@ -13,115 +13,12 @@ class StartPage extends StatelessWidget {
           title: const Text("Glose App"),
           automaticallyImplyLeading: false,
         ),
-        body: Column(children: [
-          Center(
-            child: Card(
-              clipBehavior: Clip.hardEdge,
-              child: InkWell(
-                  splashColor: const Color(0xff6750a4).withAlpha(30),
-                  onTap: () {
-                    debugPrint("Tapped");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const GamePage(
-                            questionCount: 0,
-                            scoreCount: 0,
-                            responseText: "",
-                            weekInput: "All",
-                          ),
-                        ));
-                  },
-                  child: const SizedBox(
-                    width: 300,
-                    height: 100,
-                    child: Center(
-                        child:
-                            Text("Start Game - All Weeks", style: TextStyle(fontSize: 20))),
-                  )),
-            ),
-          ),
-          Center(
-            child: Card(
-              clipBehavior: Clip.hardEdge,
-              child: InkWell(
-                  splashColor: const Color(0xff6750a4).withAlpha(30),
-                  onTap: () {
-                    debugPrint("Tapped");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const GamePage(
-                            questionCount: 0,
-                            scoreCount: 0,
-                            responseText: "",
-                            weekInput: "38",
-                          ),
-                        ));
-                  },
-                  child: const SizedBox(
-                    width: 300,
-                    height: 100,
-                    child: Center(
-                        child:
-                            Text("Start Game - Week 38", style: TextStyle(fontSize: 20))),
-                  )),
-            ),
-          ),
-          Center(
-            child: Card(
-              clipBehavior: Clip.hardEdge,
-              child: InkWell(
-                  splashColor: const Color(0xff6750a4).withAlpha(30),
-                  onTap: () {
-                    debugPrint("Tapped");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const GamePage(
-                            questionCount: 0,
-                            scoreCount: 0,
-                            responseText: "",
-                            weekInput: "39",
-                          ),
-                        ));
-                  },
-                  child: const SizedBox(
-                    width: 300,
-                    height: 100,
-                    child: Center(
-                        child:
-                            Text("Start Game - Week 39", style: TextStyle(fontSize: 20))),
-                  )),
-            ),
-          ),
-          Center(
-            child: Card(
-              clipBehavior: Clip.hardEdge,
-              child: InkWell(
-                  splashColor: const Color(0xff6750a4).withAlpha(30),
-                  onTap: () {
-                    debugPrint("Tapped");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const GamePage(
-                            questionCount: 0,
-                            scoreCount: 0,
-                            responseText: "",
-                            weekInput: "40",
-                          ),
-                        ));
-                  },
-                  child: const SizedBox(
-                    width: 300,
-                    height: 100,
-                    child: Center(
-                        child:
-                            Text("Start Game - Week 40", style: TextStyle(fontSize: 20))),
-                  )),
-            ),
-          ),
+        body: const Column(children: [
+          Padding(padding: EdgeInsets.all(30)),
+          StartButton(weekInput: "All Weeks"),
+          StartButton(weekInput: "Week 38"),
+          StartButton(weekInput: "Week 39"),
+          StartButton(weekInput: "Week 40"),
         ]));
   }
 }
